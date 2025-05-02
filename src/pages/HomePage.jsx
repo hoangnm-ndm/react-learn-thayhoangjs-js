2dbede24748a06640135b6a5be28acc2c8fe86c8
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-05-02 09:15:02 +0700

    Bai 4: React Router DOM co ban

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3000/products")
+			.then((res) => res.json())
+			.then((data) => {
+				setProducts(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+
+	return <>{JSON.stringify(products)}</>;
 };
 
 export default HomePage;
